c8a8a56be7b2b425975f7d059d92edcabc633439
alonjoe <alonjoe@gmail.com>
2022-01-10 18:52:24 +0900

    project js

---
+++ app.js
@@ -0,0 +1,36 @@
+const $link = 'https://github.com';
+
+document.querySelector('.logo').addEventListener('click', e => {
+    window.open($link);
+    console.log('링크');
+});
+
+
+//프로필 사진 렌더링람수
+function renderingPhoto() {
+
+}
+
+
+
+
+
+
+const $search = document.querySelector('.search-img img');
+$search.addEventListener('click', e => {
+    
+    
+    
+    fetch('https://api.github.com/users', reqObject);
+
+
+});
+
+
+
+
+// 즉시실행 함수
+
+(function() {
+
+})();
